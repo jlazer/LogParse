b4a6dcde78f2be0f2d41fe535f16d66b7869357a
jlazer <jlazarski98@gmail.com>
2019-08-07 14:24:04 -0700

    Proof of concept in the main python file

--- parseMain.py
+++ parseMain.py
@@ -1,2 +1,18 @@
-infile = r"/home/test/Desktop/Parse/syslog.txt
+#Justin Lazarski 2019
+infile = r"/home/test/Desktop/Parse/input/testLog.txt"
+
+important = []
+keep_phrases = [ "test","important","keep me","error"]
+
+with open(infile) as f:
+	f = f.readlines()
+
+for line in f:
+	for phrase in keep_phrases:
+		if phrase in line:
+			important.append(line)
+			break
+print(important)
+
+
 
